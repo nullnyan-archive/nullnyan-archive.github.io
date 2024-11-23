bbc87658f884d3641f7d4bfb87e48e687965938b
Vito <vitoyucepi@cock.li>
2024-11-23 21:42:00 +0000

    feat: Restore image loading
    Re-enable image gallery feature from original archive.
    Create a helper script to generate images.js file.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,43 @@
 function loadImages() {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onload = function () {
-    if (!this.responseText) console.error('Could not fetch image data');
-    var container = document.getElementById('infotabs_pics');
-    var array = this.responseText.split('\n'),
-      i = 0;
+  var container = document.getElementById('infotabs_pics');
+  var i = 0;
 
-    function waitNextImage(link) {
-      setTimeout(function () {
-        if (link) {
-          link.style.visibility = 'visible';
-          link.style.opacity = 1;
-        }
-        i++;
-        loadNextImage();
-      }, 100);
-    }
-
-    function loadNextImage() {
-      if (i >= array.length) return;
-      if (!array[i]) {
-        waitNextImage();
-        return;
+  function waitNextImage(link) {
+    setTimeout(function () {
+      if (link) {
+        link.style.visibility = 'visible';
+        link.style.opacity = 1;
       }
-      var image = new Image();
-      image.src = '/archives/images/' + encodeURI(array[i]);
-      image.onload = function () {
-        var link = document.createElement('a');
-        link.classList.add('infotab-pic-link');
-        link.target = '_blank';
-        link.href = image.src;
-        link.style.visibility = 'hidden';
-        link.style.opacity = 0;
-        link.style.backgroundImage = "url('" + link.href + "')";
-        container.appendChild(link);
-        waitNextImage(link);
-      };
-      image.onerror = function () {
-        waitNextImage();
-      };
-    }
+      i++;
+      loadNextImage();
+    }, 100);
+  }
 
-    loadNextImage();
-  };
-  xmlhttp.open('GET', '/archives/images.txt', true);
-  xmlhttp.send();
+  function loadNextImage() {
+    if (i >= images.length) return;
+    if (!images[i]) {
+      waitNextImage();
+      return;
+    }
+    var image = new Image();
+    image.src = encodeURI(images[i]);
+    image.onload = function () {
+      var link = document.createElement('a');
+      link.classList.add('infotab-pic-link');
+      link.target = '_blank';
+      link.href = image.src;
+      link.style.visibility = 'hidden';
+      link.style.opacity = 0;
+      link.style.backgroundImage = "url('" + link.href + "')";
+      container.appendChild(link);
+      waitNextImage(link);
+    };
+    image.onerror = function () {
+      waitNextImage();
+    };
+  }
+
+  loadNextImage();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
---
+++ scripts/generate_images.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+main() {
+  output='assets/js/images.js'
+  echo 'var images = [' >"$output"
+  find archives/images -type f -exec file --mime-type {} \; |
+    grep -F 'image/' |
+    cut -d':' -f1 |
+    sed 's/\(.*\)/  "\/\1",/' >>"$output"
+  echo '];' >>"$output"
+}
+
+main "$@"

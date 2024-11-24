ababfd1c04b25baf9e68bb9d4b1f86f46055f8a9
Vito <vitoyucepi@cock.li>
2024-11-24 14:10:13 +0000

    refactor: Sort images generated by generate_images script

--- scripts/generate_images.sh
+++ scripts/generate_images.sh
@@ -4,6 +4,7 @@ main() {
   output='assets/js/images.js'
   echo 'var images = [' >"$output"
   find archives/images -type f -exec file --mime-type {} \; |
+    sort |
     grep -F 'image/' |
     cut -d':' -f1 |
     sed 's/\(.*\)/  "\/\1",/' >>"$output"
